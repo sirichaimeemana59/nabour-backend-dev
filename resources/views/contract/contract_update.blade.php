2e39a89f830e5f482fa86e9bb792a9588cec906b
sirichai <sirichai@okaatplus.com>
2018-12-20 12:07:58 +0700

    update contract_signno

--- resources/views/contract/contract_update.blade.php
+++ resources/views/contract/contract_update.blade.php
@@ -177,7 +177,6 @@
                     </div>
                     <input type="hidden" name="sales_id" value="{!! $quotation1->sales_id !!}">
                     <input type="hidden" name="customer_id" value="{!! $quotation1->lead_id !!}">
-                    <input type="hidden" name="quotation_id" value="{!! $quotation1->quotation_code !!}">
                     <input type="hidden" name="price" value="{!! $price !!}">
 
                     {{--@foreach ($max_cus as $row)--}}
