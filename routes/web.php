ddc08ae91c3629a17585c1f7c40552adee267d79
sirichai <sirichai@okaatplus.com>
2018-12-24 09:38:25 +0700

    update url

--- routes/web.php
+++ routes/web.php
@@ -117,14 +117,14 @@ Route::post('customer/sales/Customer_form/check', 'Sales\CustomerController@chec
 
 //Contract sign no
 Route::get('service/contract/sign/quotation/{quotation_code?}', 'RootAdmin\ContractsignController@index');
-Route::get('service/contract/sign/form/{id?}/{quo_id?}', 'RootAdmin\ContractsignController@create');
+Route::get('service/contract/sign/form/{id?}', 'RootAdmin\ContractsignController@create');
 Route::post('service/contract/sign/add', 'RootAdmin\ContractsignController@save');
 Route::post('service/contract/sign/update', 'RootAdmin\ContractsignController@update');
 //End Contract
 
 //Contract sign no Sales
-Route::get('service/sales/contract/sign/quotation/{quotation_code?}/{lead_id?}', 'Sales\ContractsignController@index');
-Route::get('service/sales/contract/sign/form/{id?}/{quo_id?}', 'Sales\ContractsignController@create');
+Route::get('service/sales/contract/sign/quotation/{quotation_code?}', 'Sales\ContractsignController@index');
+Route::get('service/sales/contract/sign/form/{id?}', 'Sales\ContractsignController@create');
 Route::post('service/sales/contract/sign/add', 'Sales\ContractsignController@save');
 Route::post('service/sales/contract/sign/update', 'Sales\ContractsignController@update');
 //End Contract Sales
