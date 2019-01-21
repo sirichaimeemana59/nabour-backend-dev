3935b4b66bd79db5dc9e9d7393bd6a448ebf3fae
sirichai <sirichai@okaatplus.com>
2019-01-21 12:40:12 +0700

    update model property

--- app/Property.php
+++ app/Property.php
@@ -4,7 +4,7 @@ namespace App;
 class Property extends GeneralModel
 {
     protected $table = 'property';
-    protected $fillable = ['id','property_name_th','property_name_en','area_size','unit_size','construction_by','address_th','address_en','street_th','street_en','province','postcode','lat','lng','about','tel','common_area_fee_type','common_area_fee_rate','min_price','max_price','property_type','tax_id','fax','address_no','juristic_person_name_th','juristic_person_name_en','common_area_fee_land_type','common_area_fee_land_rate','is_add_initial_water_meter','is_add_initial_electric_meter','document_print_type','developer_group_id','company_name','sale_contract'];
+    protected $fillable = ['id','property_name_th','property_name_en','area_size','unit_size','construction_by','address_th','address_en','street_th','street_en','province','postcode','lat','lng','about','tel','common_area_fee_type','common_area_fee_rate','min_price','max_price','property_type','tax_id','fax','address_no','juristic_person_name_th','juristic_person_name_en','common_area_fee_land_type','common_area_fee_land_rate','is_add_initial_water_meter','is_add_initial_electric_meter','document_print_type','developer_group_id','company_name','sale_contract','property_no_label'];
     // Close timestamp
 	public $timestamps = true;
 
