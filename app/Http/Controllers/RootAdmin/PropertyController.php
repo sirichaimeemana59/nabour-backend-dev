a12086c2b029669c031b2b6d0d4bce47c031cc92
sirichai <sirichai@okaatplus.com>
2019-01-22 10:33:23 +0700

    running number property

--- app/Http/Controllers/RootAdmin/PropertyController.php
+++ app/Http/Controllers/RootAdmin/PropertyController.php
@@ -307,12 +307,12 @@ class PropertyController extends Controller {
             $update = BackendProperty::find($value->id);
             $update->property_no_label = $cus;
             //dd($update);
-            $update->save();
+            //$update->save();
 
             $_update = Property::find($value->id);
             $_update->property_no_label = $value->property_no_label;
             //dump($update->toArray());
-            //$_update->save();
+            $_update->save();
         }
 
 
